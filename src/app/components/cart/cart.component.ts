220eda7a6ae86d7f03973141636ea4517f5cd463
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-08-14 01:27:04 +0300

    Login of Admin and Seller using API

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -19,7 +19,6 @@ export class CartComponent implements OnInit {
     this.getCartItems();
   }
 
-  // Getting the cartItems
   getCartItems() {
     this.loading = true;
     this.dataService.getAllCartItems().subscribe({
@@ -35,77 +34,6 @@ export class CartComponent implements OnInit {
     });
   }
 
-  // Getting the cartItem list
-  getCartItemsList(data: any) {
-    let items: any[] = [];
-    for (const datum in data) {
-      if (Object.prototype.hasOwnProperty.call(data, datum)) {
-        const item = data[datum].data;
-
-        // add each item to cart items array
-        items.push({
-          id: item.cartItem_id,
-          price: item.attributes.productDetails.data.attributes.product_price,
-          currency: item.attributes.currency,
-          name: item.attributes.productDetails.data.attributes.product_name,
-          description:
-            item.attributes.productDetails.data.attributes.product_details,
-          discountPercent: item.attributes.discount_percent,
-          imagePath:
-            item.attributes.productDetails.data.attributes.cover_image.data
-              .attributes.path,
-          quantity: item.attributes.quantity,
-          totalPrice: item.attributes.total_price,
-        });
-
-        if (!this.currency) {
-          this.currency = item.attributes.currency;
-        }
-
-        this.grandTotal += +item.attributes.total_price;
-      }
-    }
-    return items;
-  }
-
-  // plus item
-  plusItem(itemId: string, quantity: string) {
-    this.loading = true;
-    this.dataService.editCartItem(itemId, `${+quantity + 1}`).subscribe(() => {
-      this.loading = false;
-    });
-    this.getCartItems();
-  }
-
-  // minus itemcartItems: any[] = [];
-  items: any[] = [];
-  loading = false;
-  grandTotal = 0;
-  currency = '';
-
-  constructor(private dataService: DataService) {}
-
-  ngOnInit(): void {
-    this.getCartItems();
-  }
-
-  // Getting the cartItems
-  getCartItems() {
-    this.loading = true;
-    this.dataService.getAllCartItems().subscribe({
-      next: (data: any) => {
-        this.cartItems = data.data;
-        this.items = this.getCartItemsList(this.cartItems);
-        this.loading = false;
-      },
-      error: (error) => {
-        this.loading = false;
-        console.error(error);
-      },
-    });
-  }
-
-  // Getting the cartItem list
   getCartItemsList(data: any) {
     let items: any[] = [];
     for (const datum in data) {
@@ -148,22 +76,6 @@ export class CartComponent implements OnInit {
   }
 
   // minus item
-  minusItem(itemId: string, quantity: string) {
-    this.loading = true;
-    this.dataService.editCartItem(itemId, `${+quantity - 1}`).subscribe(() => {
-      this.loading = false;
-    });
-    this.getCartItems();
-  }
-
-  // delete item
-  deleteItem(itemId: string) {
-    this.loading = true;
-    this.dataService.deleteCartItem(itemId).subscribe(() => {
-      this.getCartItems();
-      this.loading = true;
-    });
-  }
   minusItem(itemId: string, quantity: string) {
     this.loading = true;
     this.dataService.editCartItem(itemId, `${+quantity - 1}`).subscribe(() => {
