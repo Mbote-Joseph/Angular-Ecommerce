7406e439f5527448619a9566f28307e6c3af4716
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-08-17 15:27:30 +0300

    Updated cat

--- src/app/modules/user/account/orders/orders.component.ts
+++ src/app/modules/user/account/orders/orders.component.ts
@@ -1,4 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+export class Orders {
+  constructor(
+    public order_id: number,
+    public shippingDetails: string,
+    public paymentDetails: string,
+    public cartProducts: string,
+    public userDetails: string,
+    public rating: number,
+    public description: string,
+    public price: number
+  ) {}
+}
 
 @Component({
   selector: 'app-orders',
@@ -7,7 +21,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
   imagePath = 'assets/images/';
-  constructor() {}
+  orders: Orders[];
+  unpaidOrders: Orders[];
+  paidOrders: Orders[];
 
-  ngOnInit(): void {}
+  constructor(private httpClient: HttpClient) {
+    this.paidOrders = [];
+    this.unpaidOrders = [];
+    this.orders = [];
+  }
+
+  ngOnInit(): void {
+    this.getPaid();
+    this.getUnpaid();
+  }
+  getUnpaid() {
+    this.httpClient
+      .get<any>(`https://peaceful-beyond-74495.herokuapp.com/api/unpaid/orders`)
+      .subscribe((response) => {
+        console.log(response);
+        this.unpaidOrders = response;
+      });
+  }
+
+  getPaid() {
+    this.httpClient
+      .get<any>(`https://peaceful-beyond-74495.herokuapp.com/api/paid/orders`)
+      .subscribe((response) => {
+        console.log(response);
+        this.unpaidOrders = response;
+      });
+  }
 }
