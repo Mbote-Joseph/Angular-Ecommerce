e3786267b6dc458606b8be397ca69a0f9842ee99
Mbote-Joseph <mbotejoseph001@gmail.com>
2021-08-18 15:12:27 +0300

    The payment checkout

--- src/app/modules/user/account/orders/checkout/payment/payment.component.ts
+++ src/app/modules/user/account/orders/checkout/payment/payment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { OrdersComponent } from '../../orders.component';
+import { DataService } from 'src/app/services/data.service';
+import { compileDeclareInjectorFromMetadata } from '@angular/compiler';
 
 export class Orders {
   constructor(public shipping_id: number, public payment_id: number) {}
@@ -12,24 +14,106 @@ export class Orders {
   styleUrls: ['./payment.component.css'],
 })
 export class PaymentComponent implements OnInit {
-  orders: Orders[];
+  cartItems: any[] = [];
+  items: any[] = [];
+  loading = false;
+  cartMeta: any = { currency: '', totalPrice: '', itemsCount: '' };
+  VAT = 1;
+  DeriveryFee = 1.2;
+  totalPriceItem = this.cartMeta.itemsCount + this.VAT + this.DeriveryFee;
 
-  constructor(private httpClient: HttpClient) {
-    this.orders = [];
-  }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.getOrders();
+    this.getCartItems();
   }
 
-  getOrders() {
-    this.httpClient
-      .get<any>(
-        'https://peaceful-beyond-74495.herokuapp.com/api/products/(api/unpaid/orders'
-      )
-      .subscribe((response) => {
-        console.log(response);
-        this.orders = response;
-      });
+  getCartItems() {
+    this.loading = true;
+    this.dataService.getAllCartItems().subscribe({
+      next: (data: any) => {
+        this.cartItems = data.data;
+        this.cartMeta = {
+          currency: data.meta.currency,
+          itemsCount: data.meta.cart_items_count,
+          totalPrice: data.meta.total_price,
+        };
+        this.items = this.getCartItemsList(this.cartItems);
+        this.loading = false;
+      },
+      error: (error) => {
+        this.loading = false;
+        console.error(error);
+      },
+    });
+  }
+
+  getCartItemsList(data: any) {
+    let items: any[] = [];
+    for (const datum in data) {
+      if (Object.prototype.hasOwnProperty.call(data, datum)) {
+        const item = data[datum].data;
+
+        // add each item to cart items array
+        items.push({
+          id: item.cartItem_id,
+          price: item.attributes.productDetails.data.attributes.product_price,
+          currency: item.attributes.currency,
+          name: item.attributes.productDetails.data.attributes.product_name,
+          description:
+            item.attributes.productDetails.data.attributes.product_details,
+          discountPercent: item.attributes.discount_percent,
+          imagePath:
+            item.attributes.productDetails.data.attributes.cover_image.data
+              .attributes.path,
+          quantity: item.attributes.quantity,
+          totalPrice: item.attributes.total_price,
+        });
+
+        // if (!this.currency) {
+        //   this.currency = item.attributes.currency;
+        // }
+
+        // this.grandTotal += +item.attributes.total_price;
+      }
+    }
+    return items;
+  }
+
+  // plus item
+  plusItem(itemId: string, quantity: string) {
+    this.loading = true;
+    this.dataService.editCartItem(itemId, `${+quantity + 1}`).subscribe(() => {
+      this.loading = false;
+      this.getCartItems();
+    });
+  }
+
+  // minus item
+  minusItem(itemId: string, quantity: string) {
+    this.loading = true;
+    this.dataService.editCartItem(itemId, `${+quantity - 1}`).subscribe(() => {
+      this.loading = false;
+      this.getCartItems();
+    });
+    // this.getCartItems();
+  }
+
+  // delete item
+  deleteItem(itemId: string) {
+    this.loading = true;
+    this.dataService.deleteCartItem(itemId).subscribe(() => {
+      this.getCartItems();
+      this.loading = true;
+    });
+  }
+
+  // delete all cart items
+  deleteAllItems() {
+    this.loading = true;
+    this.dataService.deleteCartItems().subscribe(() => {
+      this.getCartItems();
+      this.loading = true;
+    });
   }
 }
