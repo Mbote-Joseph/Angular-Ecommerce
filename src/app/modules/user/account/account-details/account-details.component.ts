184dffe5f76187ecf3f90c0d1fa12bf898ee52fe
Edwin Mambo <educhinski650@gmail.com>
2021-08-20 14:14:58 +0300

    Account details fix

--- src/app/modules/user/account/account-details/account-details.component.ts
+++ src/app/modules/user/account/account-details/account-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../_services/user.service';
 
 @Component({
@@ -10,13 +11,42 @@ export class AccountDetailsComponent implements OnInit {
   userInfo: any[] = [];
   userDetails: any[] = [];
   loading = false;
+  userDetailsForm!: FormGroup;
+  submitted = false;
 
-  constructor(private UserService: UserService) {}
+  constructor(
+    private UserService: UserService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.userDetailsForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+    });
     this.viewUserInfo();
   }
 
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.userDetailsForm.controls;
+  }
+
+  onUpdate() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.userDetailsForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.viewUserInfo();
+  }
+
+  // view user info
   viewUserInfo() {
     this.loading = true;
     this.UserService.getUserProfile().subscribe({
@@ -24,6 +54,10 @@ export class AccountDetailsComponent implements OnInit {
         this.loading = false;
         this.userInfo = data;
         this.userDetails = this.getUserInfo(this.userInfo);
+        this.f.firstName.setValue(this.userDetails[0].name.split(' ')[0]);
+        this.f.lastName.setValue(this.userDetails[0].name.split(' ')[1]);
+        this.f.email.setValue(this.userDetails[0].email);
+        this.f.phoneNumber.setValue(this.userDetails[0].phone);
       },
       error: (error) => {
         console.error(error);
